4dcd1524e07bcbd858d9119d2ed801daf09b3793
Alejandro U. Alvarez <aluralma@gmail.com>
2017-03-03 10:08:55 +0000

    add min file, improve gulp workflow

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var uglifycss = require('gulp-uglifycss');
+var concat = require('gulp-concat');
+
+var sassOptions = {};
+var sourceMapOptions = {};
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
 
 gulp.task('build', function() {
     gulp.src('src/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/'));
+      .pipe(sourcemaps.init())
+      .pipe(sass(sassOptions).on('error', sass.logError))
+      .pipe(sourcemaps.write(sourceMapOptions))
+      .pipe(autoprefixer(autoprefixerOptions))
+      .pipe(gulp.dest('dist'))
+      .pipe(uglifycss({
+        uglyComments: false
+      }))
+      .pipe(concat('ui-grid-custom.min.css'))
+      .pipe(gulp.dest('./dist/'));
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('src/**/*.scss',['build']);
 });
--- app.js
+++ app.js
@@ -2,24 +2,32 @@ var app = angular.module('app', ['ngTouch', 'ui.grid']);
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
 
-  $scope.myData = [
-    {
-        "firstName": "Cox",
-        "lastName": "Carney",
-        "company": "Enormo",
-        "employed": true
-    },
-    {
-        "firstName": "Lorraine",
-        "lastName": "Wise",
-        "company": "Comveyer",
-        "employed": false
-    },
-    {
-        "firstName": "Nancy",
-        "lastName": "Waters",
-        "company": "Fuelton",
-        "employed": false
-    }
-  ];
+  $scope.gridConfig = {
+    enableCellEditOnFocus: false,
+    enableFiltering: true,
+    multiSelect: false,
+    enableGridMenu: true,
+    paginationPageSizes: [25, 50, 75],
+    paginationPageSize: 10,
+    data: [
+      {
+          "firstName": "Cox",
+          "lastName": "Carney",
+          "company": "Enormo",
+          "employed": true
+      },
+      {
+          "firstName": "Lorraine",
+          "lastName": "Wise",
+          "company": "Comveyer",
+          "employed": false
+      },
+      {
+          "firstName": "Nancy",
+          "lastName": "Waters",
+          "company": "Fuelton",
+          "employed": false
+      }
+    ],
+  };
 }]);
